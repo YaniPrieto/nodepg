04910a3976f6641a085a4f64b199830e4c339188
yani <54736274+YaniPrieto@users.noreply.github.com>
2020-02-27 01:40:25 +0800

    Add files via upload

---
+++ prieto-problem2.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+ const pool = new Pool({
+ user: `${process.env.DB_USER}`,
+ host: `${process.env.DB_HOST}`,
+ database: `${process.env.DB_DATABASE}`,
+ password: `${process.env.DB_PASSWORD}`,
+ port: process.env.DB_PORT,
+ ssl: true,
+ })
+
+ pool.query(`SELECT occupation, name, class
+ from pets
+ INNER JOIN owners
+ ON pets.owner_id = owners.owner_id
+ WHERE class = 'Bird'`, (error, results) => {
+ if (error) {
+ throw error
+ }
+ console.log(results.rows)
+ })
+
